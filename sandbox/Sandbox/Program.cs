85b2e8fca6a5242100d4e4825eb026c3e5d3972f
Noon015 <cpbohman@gmail.com>
2025-09-16 11:05:55 -0600

    Added Things and Stuff. You know the stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Does this work like i think it does?");
+
+        int num = 2;
+
+        Console.WriteLine($"My number is {num}");
     }
 }
