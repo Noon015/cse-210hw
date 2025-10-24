4c41fade54ebc5a6925fed0e14092fa60e04cf0d
Noon015 <cpbohman@gmail.com>
2025-10-24 00:13:20 -0600

    Word length bug fixed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,12 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Refrence myRef = new Refrence("Isaiah", 5, 20);
-        string scriptureText = "Woe unto them that call evil\ngood and good evil;\nthat put darkness for light and\nlight for darkness;\nthat call bitter sweet,\nand sweet bitter";
+        string scriptureText = "Woe unto them that call evil good and good evil; that put darkness for light and light for darkness; that call bitter sweet, and sweet bitter";
         Scripture myScripture = new Scripture(scriptureText, myRef);
         Console.Clear();
         while (true)
         {
             Console.WriteLine(myScripture.DisplayScripture());
+            Console.WriteLine();
             Console.WriteLine("Press enter to continue or type quit to exit");
             string answer = Console.ReadLine();
 
